b5e2205541817d84b740c49944713fb595cc23f9
Miltonsms <milsmsramirezz@gmail.com>
2015-01-29 11:24:05 -0600

    test

---
+++ js/tests.js
@@ -0,0 +1,7 @@
+describe('funcionamienot de proyecto con angular.', function() {
+  var  $scope;
+	
+    it('verificacion de funcion de ordenby', function() {
+      expect(false).not.toBe(true);
+      });
+});
